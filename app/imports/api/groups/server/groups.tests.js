bcf947f1cfcbc1b0395abeaf10d3399c9cfef952
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2019-11-21 15:37:48 +0100

    add new tests on groups/roles

--- app/imports/api/groups/server/groups.tests.js
+++ app/imports/api/groups/server/groups.tests.js
@@ -5,9 +5,21 @@ import { Factory } from "meteor/dburles:factory";
 import { PublicationCollector } from "meteor/johanbrook:publication-collector";
 import { chai, assert } from "meteor/practicalmeteor:chai";
 import { Random } from "meteor/random";
+import faker from "faker";
 import { Meteor } from "meteor/meteor";
 import { _ } from "meteor/underscore";
 import { Groups } from "../groups.js";
+
+// this file also includes tests on users/permissions
+import { Accounts } from "meteor/accounts-base";
+import { Roles } from "meteor/alanning:roles";
+
+import {
+  setAdminOf,
+  unsetAdminOf,
+  setMemberOf,
+  unsetMemberOf
+} from "../../users/methods";
 import { createGroup, removeGroup } from "../methods.js";
 import "/i18n/en.i18n.json";
 import "./publications.js";
@@ -24,8 +36,7 @@ describe("groups", function() {
     const userId = Random.id();
     before(function() {
       Groups.remove({});
-      _.times(2, () => Factory.create("group", { owner: userId }));
-      _.times(2, () => Factory.create("group", { owner: Random.id() }));
+      _.times(4, () => Factory.create("group", { owner: Random.id() }));
     });
     describe("groups.all", function() {
       it("sends all groups", function(done) {
@@ -39,18 +50,193 @@ describe("groups", function() {
   });
   describe("methods", function() {
     let groupId;
+    let group2Id;
+    let group3Id;
     let userId;
+    let adminId;
+    let otherUserId;
     beforeEach(function() {
       // Clear
       Groups.remove({});
-      // Generate a 'user'
-      userId = Random.id();
-      adminId = Random.id();
+      Meteor.users.remove({});
+      // FIXME : find a way to reset roles collection ?
+      Roles.createRole("admin", { unlessExists: true });
+      Roles.createRole("member", { unlessExists: true });
+      // Generate 'users'
+      const email = faker.internet.email();
+      userId = Accounts.createUser({
+        email,
+        username: email,
+        password: "toto"
+      });
+      const emailAdmin = faker.internet.email();
+      adminId = Accounts.createUser({
+        email: emailAdmin,
+        username: emailAdmin,
+        password: "toto"
+      });
+      const emailOtherUser = faker.internet.email();
+      otherUserId = Accounts.createUser({
+        email: emailOtherUser,
+        username: emailOtherUser,
+        password: "toto"
+      });
+      // set this user as global admin
+      Roles.addUsersToRoles(adminId, "admin");
       // Create a group owned by userId
       groupId = Factory.create("group", { owner: userId })._id;
-      // Create a group owned by randim user and set adminId as admin
-      groupAdminId = Factory.create("group", { owner: Random.id() })._id;
-      Groups.update(groupAdminId, { $push: { admins: adminId } });
+      // Create a group owned by random user and set userId as admin
+      group2Id = Factory.create("group", { owner: Random.id() })._id;
+      setAdminOf._execute({ userId: adminId }, { userId, groupId: group2Id });
+      group3Id = Factory.create("group", { owner: Random.id() })._id;
+    });
+    describe("(un)setAdminOf", function() {
+      it("global admin can set/unset a user as admin of a group", function() {
+        // Throws if non owner/admin user, or logged out user, tries to delete the group
+        setAdminOf._execute({ userId: adminId }, { userId, groupId: group3Id });
+        let group = Groups.findOne(group3Id);
+        assert.equal(Roles.userIsInRole(userId, "admin", group3Id), true);
+        assert.include(
+          group.admins,
+          userId,
+          "group admins list contains userId"
+        );
+        unsetAdminOf._execute(
+          { userId: adminId },
+          { userId, groupId: group3Id }
+        );
+        group = Groups.findOne(group3Id);
+        assert.equal(Roles.userIsInRole(userId, "admin", group3Id), false);
+        assert.notInclude(
+          group.admins,
+          userId,
+          "group admins list shouldn't contain userId"
+        );
+      });
+      it("group admin can set/unset a user as admin of a group", function() {
+        setAdminOf._execute(
+          { userId },
+          { userId: otherUserId, groupId: group2Id }
+        );
+        let group = Groups.findOne(group2Id);
+        assert.equal(Roles.userIsInRole(otherUserId, "admin", group2Id), true);
+        assert.include(
+          group.admins,
+          otherUserId,
+          "group admins list contains otherUserId"
+        );
+        unsetAdminOf._execute(
+          { userId },
+          { userId: otherUserId, groupId: group2Id }
+        );
+        group = Groups.findOne(group2Id);
+        assert.equal(Roles.userIsInRole(otherUserId, "admin", group2Id), false);
+        assert.notInclude(
+          group.admins,
+          otherUserId,
+          "group admins list shouldn't contain otherUserId"
+        );
+      });
+      it("only global or group admin can set/unset a user as admin of a group", function() {
+        // Throws if non owner/admin user, or logged out user, tries to delete the group
+        assert.throws(
+          () => {
+            setAdminOf._execute(
+              { userId: otherUserId },
+              { userId, groupId: groupId }
+            );
+          },
+          Meteor.Error,
+          /api.users.setAdminOf.notPermitted/
+        );
+        assert.throws(
+          () => {
+            unsetAdminOf._execute(
+              { userId: otherUserId },
+              { userId, groupId: groupId }
+            );
+          },
+          Meteor.Error,
+          /api.users.unsetAdminOf.notPermitted/
+        );
+      });
+    });
+    describe("setMemberOf", function() {
+      it("global admin can set/unset a user as member of a group", function() {
+        setMemberOf._execute(
+          { userId: adminId },
+          { userId, groupId: group3Id }
+        );
+        let group = Groups.findOne(group3Id);
+        assert.equal(Roles.userIsInRole(userId, "member", group3Id), true);
+        assert.include(
+          group.members,
+          userId,
+          "group members list contains userId"
+        );
+        unsetMemberOf._execute(
+          { userId: adminId },
+          { userId, groupId: group3Id }
+        );
+        group = Groups.findOne(group3Id);
+        assert.equal(Roles.userIsInRole(userId, "member", group3Id), false);
+        assert.notInclude(
+          group.members,
+          userId,
+          "group members list shouldn't contain userId"
+        );
+      });
+      it("group admin can set/unset a user as member of a group", function() {
+        // Throws if non owner/admin user, or logged out user, tries to delete the group
+        setMemberOf._execute(
+          { userId },
+          { userId: otherUserId, groupId: group2Id }
+        );
+        let group = Groups.findOne(group2Id);
+        assert.equal(Roles.userIsInRole(otherUserId, "member", group2Id), true);
+        assert.include(
+          group.members,
+          otherUserId,
+          "group members list contains otherUserId"
+        );
+        unsetMemberOf._execute(
+          { userId },
+          { userId: otherUserId, groupId: group2Id }
+        );
+        group = Groups.findOne(group2Id);
+        assert.equal(
+          Roles.userIsInRole(otherUserId, "member", group2Id),
+          false
+        );
+        assert.notInclude(
+          group.members,
+          otherUserId,
+          "group members list shouldn't contain otherUserId"
+        );
+      });
+      it("only global or group admin can set/unset a user as member of a group", function() {
+        // Throws if non owner/admin user, or logged out user, tries to delete the group
+        assert.throws(
+          () => {
+            setMemberOf._execute(
+              { userId: otherUserId },
+              { userId, groupId: groupId }
+            );
+          },
+          Meteor.Error,
+          /api.users.setMemberOf.notPermitted/
+        );
+        assert.throws(
+          () => {
+            unsetMemberOf._execute(
+              { userId: otherUserId },
+              { userId, groupId: groupId }
+            );
+          },
+          Meteor.Error,
+          /api.users.unsetMemberOf.notPermitted/
+        );
+      });
     });
     describe("createGroup", function() {
       it("does create a group and set current user as owner", function() {
@@ -70,7 +256,7 @@ describe("groups", function() {
         // Throws if non owner/admin user, or logged out user, tries to delete the group
         assert.throws(
           () => {
-            removeGroup._execute({ userId: adminId }, { groupId });
+            removeGroup._execute({ userId: userId }, { groupId: group3Id });
           },
           Meteor.Error,
           /api.groups.removeGroup.notPermitted/
@@ -82,6 +268,18 @@ describe("groups", function() {
           Meteor.Error,
           /api.groups.removeGroup.notPermitted/
         );
+      });
+      it("does delete a group you own", function() {
+        removeGroup._execute({ userId }, { groupId });
+        assert.equal(Groups.findOne(groupId), undefined);
+      });
+      it("does delete a group you are admin of", function() {
+        removeGroup._execute({ userId }, { groupId: group2Id });
+        assert.equal(Groups.findOne(group2Id), undefined);
+      });
+      it("does delete any group when you are global admin", function() {
+        removeGroup._execute({ userId: adminId }, { groupId: group3Id });
+        assert.equal(Groups.findOne(group3Id), undefined);
       });
     });
   });
