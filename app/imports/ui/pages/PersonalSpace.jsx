d083d21da43f21a15927e215fe71fee1e1526d97
Francois AUBEUT <contact@mexar.fr>
2020-02-17 10:44:08 +0000

    Display favorites on personal space

--- app/imports/ui/pages/PersonalSpace.jsx
+++ app/imports/ui/pages/PersonalSpace.jsx
@@ -1,41 +1,103 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { Meteor } from 'meteor/meteor';
+import PropTypes from 'prop-types';
+import { withTracker } from 'meteor/react-meteor-data';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
 import i18n from 'meteor/universe:i18n';
-import {
-  Typography, Container, Grid, makeStyles, Fade,
-} from '@material-ui/core';
-import clsx from 'clsx';
 
-// import { Context } from '../contexts/context';
+import { Typography, Fade } from '@material-ui/core';
+import ServiceDetails from '../components/ServiceDetails';
+import Services from '../../api/services/services';
+import Spinner from '../components/Spinner';
+import { Context } from '../contexts/context';
+import Categories from '../../api/categories/categories';
 
 const useStyles = makeStyles((theme) => ({
   cardGrid: {
     paddingTop: theme.spacing(5),
     paddingBottom: theme.spacing(5),
   },
-  welcome: {
+  chip: {
+    margin: theme.spacing(1),
+  },
+  badge: { position: 'inherit' },
+  gridItem: {
     display: 'flex',
-    flexDirection: 'column',
     justifyContent: 'center',
-    alignItems: 'center',
   },
 }));
 
-const PersonalSpace = () => {
-  //   const [{ user, loadingUser }] = useContext(Context);
+function ServicesPage({ services, ready, categories }) {
   const classes = useStyles();
+  const [{ user, loadingUser }] = useContext(Context);
+  const favs = loadingUser ? [] : user.favServices;
+  const [catList, setCatList] = useState([]);
+
+  const updateCatList = (catId) => {
+    // Call by click on categories of services
+    if (catList.includes(catId)) {
+      // catId already in list so remove it
+      setCatList(catList.filter((id) => id !== catId));
+    } else {
+      // add new catId to list
+      setCatList([...catList, catId]);
+    }
+  };
+
+  const filterServices = (service) => favs.find((serviceId) => serviceId === service._id);
 
   return (
-    <Fade in>
-      <Container className={clsx(classes.cardGrid, 'page')}>
-        <Grid container spacing={4}>
-          <Grid item xs={12} sm={12} md={12} className={classes.welcome}>
-            <Typography variant="h4">{i18n.__('pages.PersonalSpace.welcome')}</Typography>
-            <img src="http://waynecountyfairohio.com/wp-content/uploads/2015/03/ComingSoon.png" alt="coming soon" />
-          </Grid>
-        </Grid>
-      </Container>
-    </Fade>
+    <>
+      {!ready ? (
+        <Spinner />
+      ) : (
+        <Fade in>
+          <Container className={classes.cardGrid}>
+            <Grid container spacing={4}>
+              <Grid item xs={12} sm={12} md={12}>
+                <Typography variant="h4">{i18n.__('pages.PersonalSpace.welcome')}</Typography>
+              </Grid>
+              {services
+                .filter((service) => filterServices(service))
+                .map((service) => {
+                  const favAction = favs.indexOf(service._id) === -1 ? 'fav' : 'unfav';
+                  return (
+                    <Grid className={classes.gridItem} item key={service._id} xs={12} sm={6} md={4} lg={3}>
+                      <ServiceDetails
+                        service={service}
+                        favAction={favAction}
+                        categories={categories}
+                        updateCategories={updateCatList}
+                        catList={catList}
+                      />
+                    </Grid>
+                  );
+                })}
+            </Grid>
+          </Container>
+        </Fade>
+      )}
+    </>
   );
+}
+
+ServicesPage.propTypes = {
+  services: PropTypes.arrayOf(PropTypes.object).isRequired,
+  categories: PropTypes.arrayOf(PropTypes.object).isRequired,
+  ready: PropTypes.bool.isRequired,
 };
 
-export default PersonalSpace;
+export default withTracker(() => {
+  const servicesHandle = Meteor.subscribe('services.all');
+  const categoriesHandle = Meteor.subscribe('categories.all');
+  const services = Services.find({}, { sort: { title: 1 } }).fetch();
+  const ready = servicesHandle.ready() && categoriesHandle.ready();
+  const categories = Categories.find({}, { sort: { name: 1 } }).fetch();
+  return {
+    services,
+    categories,
+    ready,
+  };
+})(ServicesPage);
