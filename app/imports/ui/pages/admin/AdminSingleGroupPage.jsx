e4034d242e1f1a5afca1059e65e2ec121d8187eb
Francois <contact@mexar.fr>
2020-04-15 11:26:12 +0200

    add options to components to delete whitespace

--- app/imports/ui/pages/admin/AdminSingleGroupPage.jsx
+++ app/imports/ui/pages/admin/AdminSingleGroupPage.jsx
@@ -221,7 +221,16 @@ const AdminSingleGroupPage = ({ group, ready, match: { params } }) => {
             />
             <div className={classes.wysiwyg}>
               <InputLabel htmlFor="content">{i18n.__('pages.AdminSingleGroupPage.content')}</InputLabel>
-              <ReactQuill id="content" value={content} onChange={onUpdateRichText} />
+              <ReactQuill
+                id="content"
+                value={content}
+                onChange={onUpdateRichText}
+                modules={{
+                  clipboard: {
+                    matchVisual: false,
+                  },
+                }}
+              />
             </div>
             {params._id ? (
               // user management is not possible when creating a new group
