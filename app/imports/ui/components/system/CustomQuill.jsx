addaaf09f9d01d078729ee100c759d61851605ee
Francois AUBEUT <contact@mexar.fr>
2020-07-06 08:42:45 +0000

    Webcam wysiwyg

--- app/imports/ui/components/system/CustomQuill.jsx
+++ app/imports/ui/components/system/CustomQuill.jsx
@@ -1,9 +1,12 @@
+// eslint-disable max-len
 import React from 'react';
 import PropTypes from 'prop-types';
 import i18n from 'meteor/universe:i18n';
 import Tooltip from '@material-ui/core/Tooltip';
 
-function CustomToolbarArticle({ withMedia }) {
+const isSafari = () => /^((?!chrome|android).)*safari/i.test(window.navigator.userAgent);
+
+function CustomToolbarArticle({ withMedia, withWebcam }) {
   return (
     <div id="quill-toolbar">
       <Tooltip
@@ -19,7 +22,7 @@ function CustomToolbarArticle({ withMedia }) {
             <option value="3" aria-labelledby="header-tooltip" />
             <option value="4" aria-labelledby="header-tooltip" />
             <option value="5" aria-labelledby="header-tooltip" />
-            <option selected="selected" aria-labelledby="header-tooltip" />
+            <option value="" aria-labelledby="header-tooltip" />
           </select>
         </span>
       </Tooltip>
@@ -128,6 +131,29 @@ function CustomToolbarArticle({ withMedia }) {
             <button type="button" className="ql-video" aria-labelledby="video-tooltip" />
           </Tooltip>
         ) : null}
+        {withWebcam && !isSafari() ? (
+          <Tooltip
+            id="webcam-tooltip"
+            arrow
+            title={i18n.__('components.CustomQuill.webcam')}
+            aria-label={i18n.__('components.CustomQuill.webcam')}
+          >
+            <button type="button" className="ql-webcam" aria-labelledby="webcam-tooltip">
+              <svg id="webcam-icon" enableBackground="new 0 0 512.037 512.037" viewBox="0 0 512.037 512.037">
+                <g>
+                  {/* eslint-disable-next-line max-len */}
+                  <path d="m256.018 263.018c39.149 0 71-31.851 71-71s-31.851-71-71-71-71 31.851-71 71 31.851 71 71 71zm0-112c22.607 0 41 18.393 41 41s-18.393 41-41 41-41-18.393-41-41 18.393-41 41-41z" />
+                  {/* eslint-disable-next-line max-len */}
+                  <path d="m256.018 343.018c83.262 0 151-67.738 151-151s-67.738-151-151-151-151 67.738-151 151 67.738 151 151 151zm0-272c66.72 0 121 54.28 121 121s-54.28 121-121 121-121-54.28-121-121 54.28-121 121-121z" />
+                  {/* eslint-disable-next-line max-len */}
+                  <path d="m241.308 194.948c1.675 8.08 9.315 13.34 17.64 11.78 8.156-1.707 13.326-9.376 11.78-17.641-1.864-8.887-10.663-13.919-19.07-11.42-7.849 2.567-11.729 9.923-10.35 17.281z" />
+                  {/* eslint-disable-next-line max-len */}
+                  <path d="m508.683 446.589c-103.445-127.973-97.297-120.707-99.434-122.38-5.662-7.23-16.155-7.638-22.417-1.376-72.299 72.299-189.323 72.306-261.629 0-6.272-6.274-16.765-5.841-22.417 1.376-2.038 1.596 4.84-6.619-99.434 122.38-7.914 9.79-.929 24.43 11.665 24.43h482c12.59-.001 19.583-14.636 11.666-24.43zm-462.252-5.571 69.456-85.926c79.909 68.884 199.459 69.654 280.262 0l69.456 85.926z" />
+                </g>
+              </svg>
+            </button>
+          </Tooltip>
+        ) : null}
       </span>
       <span className="ql-formats">
         <Tooltip
@@ -145,10 +171,12 @@ function CustomToolbarArticle({ withMedia }) {
 
 CustomToolbarArticle.defaultProps = {
   withMedia: false,
+  withWebcam: false,
 };
 
 CustomToolbarArticle.propTypes = {
   withMedia: PropTypes.bool,
+  withWebcam: PropTypes.bool,
 };
 
 function CustomToolbar() {
