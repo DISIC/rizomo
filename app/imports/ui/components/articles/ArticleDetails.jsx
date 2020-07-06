146c21cc91c702f2c588d5a43665b98d2d227069
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2020-07-06 12:13:07 +0200

    Make author and article cards globally clickable

--- app/imports/ui/components/articles/ArticleDetails.jsx
+++ app/imports/ui/components/articles/ArticleDetails.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import Tooltip from '@material-ui/core/Tooltip';
-import { Button, CardHeader, IconButton, CardContent, Typography } from '@material-ui/core';
+import { Button, CardHeader, IconButton, CardContent, Typography, CardActionArea } from '@material-ui/core';
 import i18n from 'meteor/universe:i18n';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import EditIcon from '@material-ui/icons/Edit';
@@ -45,6 +45,12 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ArticleDetails({ article, publicPage }) {
   const classes = useStyles();
+  const history = useHistory();
+
+  const handlePublic = () => {
+    history.push(`/public/${article.userId}/${article.slug}`);
+  };
+
   const actionButtons = (
     <div style={{ display: 'flex' }}>
       <Tooltip
@@ -74,30 +80,48 @@ export default function ArticleDetails({ article, publicPage }) {
   );
 
   const publicButton = (
-    <Tooltip title={i18n.__('components.ArticleDetails.open')} aria-label={i18n.__('components.ArticleDetails.open')}>
-      <Link to={`/public/${article.userId}/${article.slug}`}>
-        <IconButton color="primary">
-          <ChevronRightIcon fontSize="large" />
-        </IconButton>
-      </Link>
-    </Tooltip>
+    <IconButton color="primary" onClick={handlePublic}>
+      <ChevronRightIcon fontSize="large" />
+    </IconButton>
   );
+
+  const cardHeader = (
+    <CardHeader
+      classes={{ action: classes.action }}
+      action={publicPage ? publicButton : actionButtons}
+      title={article.title}
+      titleTypographyProps={{
+        variant: 'h6',
+        color: 'primary',
+      }}
+      subheader={`${i18n.__('components.ArticleDetails.publishedOn')} ${article.createdAt.toLocaleString()} `}
+      subheaderTypographyProps={{ variant: 'body2', color: 'primary' }}
+    />
+  );
+
+  const cardContent = (
+    <CardContent>
+      <Typography paragraph>{article.description || i18n.__('components.ArticleDetails.noDescription')}</Typography>
+    </CardContent>
+  );
+
   return (
     <Card className={classes.card} elevation={3}>
-      <CardHeader
-        classes={{ action: classes.action }}
-        action={publicPage ? publicButton : actionButtons}
-        title={<Link to={`/public/${article.userId}/${article.slug}`}>{article.title}</Link>}
-        titleTypographyProps={{
-          variant: 'h6',
-          color: 'primary',
-        }}
-        subheader={`${i18n.__('components.ArticleDetails.publishedOn')} ${article.createdAt.toLocaleString()} `}
-        subheaderTypographyProps={{ variant: 'body2', color: 'primary' }}
-      />
-      <CardContent>
-        <Typography paragraph>{article.description || i18n.__('components.ArticleDetails.noDescription')}</Typography>
-      </CardContent>
+      {publicPage ? (
+        <CardActionArea
+          title={i18n.__('components.ArticleDetails.open')}
+          aria-label={i18n.__('components.ArticleDetails.open')}
+          onClick={handlePublic}
+        >
+          {cardHeader}
+          {cardContent}
+        </CardActionArea>
+      ) : (
+        <>
+          {cardHeader}
+          {cardContent}
+        </>
+      )}
     </Card>
   );
 }
