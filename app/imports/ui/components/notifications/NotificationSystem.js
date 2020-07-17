3999bb8e8abc795b5333040d6b4f2f9af46674de
Edouard Le Bourgeois <edouard.le-bourgeois@ac-dijon.fr>
2020-07-17 10:01:55 +0000

    Revert "package-lock.json update"
    This reverts commit 567a787f70693ba65c8a432cce9a7de64d851f26.

---
+++ app/imports/ui/components/notifications/NotificationSystem.js
@@ -0,0 +1,16 @@
+export default function notificationSystem(title, options) {
+  if (Notification.permission === 'granted') {
+    // eslint-disable-next-line
+    const notification = new Notification(title, options);
+  } else if (Notification.permission !== 'denied') {
+    Notification.requestPermission(function (permission) {
+      if (!('permission' in Notification)) {
+        Notification.permission = permission;
+      }
+      if (permission === 'granted') {
+        // eslint-disable-next-line
+        const notification = new Notification(title, options);
+      }
+    });
+  }
+}
