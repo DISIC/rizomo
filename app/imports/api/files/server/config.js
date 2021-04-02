a4be588b486b5a29f62a1df2ee53c07a5bb41fba
Lionel Morin <lionel.morin@ac-dijon.fr>
2021-04-02 15:24:11 +0000

    Background and Sample

--- app/imports/api/files/server/config.js
+++ app/imports/api/files/server/config.js
@@ -2,7 +2,8 @@ import Minio from 'minio';
 
 // Instantiate the minio client with the endpoint
 // and access keys as shown below.
-const { minioEndPoint, minioPort, minioSSL, minioAccess } = Meteor.settings.public;
+const { minioEndPoint, minioPort, minioSSL } = Meteor.settings.public;
+const { minioAccess } = Meteor.settings.private;
 const s3Client = new Minio.Client({
   endPoint: minioEndPoint,
   port: minioPort,
