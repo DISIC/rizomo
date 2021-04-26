ca416ddf3777aec630c5a1c6808f052be79d90be
Bruno Boiget <bruno.boiget@ac-dijon.fr>
2021-04-26 10:42:08 +0200

    Fix external links check on Footer

--- app/imports/ui/components/menus/Footer.jsx
+++ app/imports/ui/components/menus/Footer.jsx
@@ -57,23 +57,23 @@ const Footer = () => {
           return isMobile ? (
             <li key={key} className={classes.li}>
               {external ? (
-                <Link className={classes.mobileLink} to={`/legal/${link}`}>
-                  {i18n.__(`components.Footer.${text}`)}
-                </Link>
-              ) : (
                 <a className={classes.mobileLink} href={link} target="_blank" rel="noreferrer noopener">
                   {i18n.__(`components.Footer.${text}`)}
                 </a>
+              ) : (
+                <Link className={classes.mobileLink} to={`/legal/${link}`}>
+                  {i18n.__(`components.Footer.${text}`)}
+                </Link>
               )}
             </li>
           ) : external ? (
-            <Link key={key} className={classes.link} to={`/legal/${link}`}>
-              {i18n.__(`components.Footer.${text}`)}
-            </Link>
-          ) : (
             <a key={key} className={classes.link} href={link} target="_blank" rel="noreferrer noopener">
               {i18n.__(`components.Footer.${text}`)}
             </a>
+          ) : (
+            <Link key={key} className={classes.link} to={`/legal/${link}`}>
+              {i18n.__(`components.Footer.${text}`)}
+            </Link>
           );
         })}
       </>
