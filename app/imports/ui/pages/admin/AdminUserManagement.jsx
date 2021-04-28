02b8c4314f94107de063888728655cbf8d8e93bb
Corentin Faisy <corentin.faisy@ac-dijon.fr>
2021-04-28 13:53:36 +0200

    WIP: Page user management

---
+++ app/imports/ui/pages/admin/AdminUserManagement.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+
+const AdminUserManagement = () => {
+  return <p>Hello World</p>;
+};
+
+AdminUserManagement.propTypes = {};
+
+export default withTracker(() => {})(AdminUserManagement);
