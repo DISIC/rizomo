e734a00ef067fa6837b9f9df75ff4f70d4c8efa5
Léo <leo.moscillo@mail.com>
2021-05-18 10:17:48 +0200

    responsive iframe

--- app/imports/ui/components/screencast/ScreencastGroup.jsx
+++ app/imports/ui/components/screencast/ScreencastGroup.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
+import { useAppContext } from '../../contexts/context';
 
 export default function ScreencastGroup() {
+  const [{ isMobile }] = useAppContext();
   const useStyles = makeStyles({
-    iframe: {
+    grid: {
       display: 'grid',
       justifyContent: 'center',
       padding: '20px',
-      width: '55vw',
-      height: '55vh',
+    },
+    iframe: {
+      width: isMobile ? '90vw' : '55vw',
+      height: isMobile ? '50vmin' : '55vh',
     },
   });
   const classes = useStyles();
 
   return (
-    <Grid className={classes.iframe}>
+    <Grid className={classes.grid}>
       <iframe
+        className={classes.iframe}
         title="screencast_frame"
         sandbox="allow-same-origin allow-scripts allow-popups"
         src="https://tube-dijon.beta.education.fr/videos/embed/57752b90-5b36-4b3f-9b83-1b7464e41a5f"
