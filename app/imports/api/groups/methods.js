ad026fc97a5fc8c680096656d6b05ba97d02c414
Corentin Faisy <corentin.faisy@ac-dijon.fr>
2021-05-19 14:30:23 +0000

    Group import group

--- app/imports/api/groups/methods.js
+++ app/imports/api/groups/methods.js
@@ -5,7 +5,6 @@ import SimpleSchema from 'simpl-schema';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { Roles } from 'meteor/alanning:roles';
 import i18n from 'meteor/universe:i18n';
-
 import { isActive, getLabel } from '../utils';
 import Groups from './groups';
 import { addGroup, removeElement } from '../personalspaces/methods';
@@ -60,6 +59,113 @@ export const unfavGroup = new ValidatedMethod({
     }
     // update user personalSpace
     removeElement._execute({ userId: this.userId }, { type: 'group', elementId: groupId });
+  },
+});
+
+export const findGroups = new ValidatedMethod({
+  name: 'groups.findGroups',
+  validate: new SimpleSchema({
+    page: {
+      type: SimpleSchema.Integer,
+      min: 1,
+      defaultValue: 1,
+      optional: true,
+      label: getLabel('api.methods.labels.page'),
+    },
+    pageSize: {
+      type: SimpleSchema.Integer,
+      min: 1,
+      defaultValue: 10,
+      optional: true,
+      label: getLabel('api.methods.labels.pageSize'),
+    },
+    sortColumn: {
+      type: String,
+      defaultValue: 'name',
+      optional: true,
+      label: getLabel('api.methods.labels.sortColumn'),
+    },
+    sortOrder: {
+      type: SimpleSchema.Integer,
+      allowedValues: [1, -1],
+      defaultValue: 1,
+      optional: true,
+      label: getLabel('api.methods.labels.sortOrder'),
+    },
+    groupId: {
+      type: String,
+      optional: true,
+    },
+  }).validator({ clean: true }),
+  run({ page, pageSize, sortColumn, sortOrder, groupId }) {
+    const isAdmin = Roles.userIsInRole(this.userId, 'admin');
+    const user = Meteor.users.findOne({ _id: this.userId });
+    // calculate number of entries to skip
+    const skip = (page - 1) * pageSize;
+    const myGroups = user.favGroups;
+    const sort = {};
+    sort[sortColumn] = sortOrder;
+    const totalCount = Groups.find({ _id: { $in: myGroups, $ne: groupId } }).count();
+    const data = Groups.find(
+      { _id: { $in: myGroups, $ne: groupId } },
+      {
+        fields: isAdmin ? Groups.adminFields : Groups.publicFields,
+        limit: pageSize,
+        skip,
+        sort,
+      },
+    ).fetch();
+    return { data, page, totalCount };
+  },
+});
+
+export const addGroupMembersToGroup = new ValidatedMethod({
+  name: 'groups.addGroupMembersToGroup',
+  validate: new SimpleSchema({
+    groupId: { type: String, regEx: SimpleSchema.RegEx.Id, label: getLabel('api.groups.labels.id') },
+    otherGroupId: { type: String, regEx: SimpleSchema.RegEx.Id, label: getLabel('api.groups.labels.id') },
+  }).validator(),
+
+  run({ groupId, otherGroupId }) {
+    // check group and user existence
+    const group = Groups.findOne({ _id: groupId });
+    const group2 = Groups.findOne({ _id: otherGroupId });
+    if (group === undefined || group2 === undefined) {
+      throw new Meteor.Error('api.groups.addGroupMemberToGroup.unknownGroup', i18n.__('api.groups.unknownGroup'));
+    }
+    // check if current user has admin rights on group (or global admin)
+    const authorized =
+      (isActive(this.userId) && Roles.userIsInRole(this.userId, 'admin', groupId)) ||
+      (this.userId === group.owner && this.userId === group2.owner);
+    if (!authorized) {
+      throw new Meteor.Error('api.groups.addGroupMemberToGroup.notPermitted', i18n.__('api.groups.adminGroupNeeded'));
+    }
+
+    const usersGroup = group2.members;
+
+    let nb = 0;
+    let i;
+    for (i = 0; i < usersGroup.length; i += 1) {
+      // add role to user collection
+      if (!Roles.userIsInRole(usersGroup[i], 'member', groupId)) {
+        Roles.addUsersToRoles(usersGroup[i], 'member', groupId);
+        // remove candidate Role if present
+        if (Roles.userIsInRole(usersGroup[i], 'candidate', groupId)) {
+          Roles.removeUsersFromRoles(usersGroup[i], 'candidate', groupId);
+        }
+        // store info in group collection
+        if (group.members.indexOf(usersGroup[i]) === -1) {
+          Groups.update(groupId, {
+            $push: { members: usersGroup[i] },
+            $pull: { candidates: usersGroup[i] },
+          });
+        }
+        // update user personalSpace
+        favGroup._execute({ userId: usersGroup[nb] }, { groupId });
+        nb += 1;
+      }
+    }
+    return nb;
   },
 });
 
