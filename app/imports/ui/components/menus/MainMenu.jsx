d8de75b724715aa225d2d611e99022cd4765c46c
cfaisy <corentin.faisy@gmail.com>
2021-10-12 15:03:24 +0200

    Disabled button for app version in main menu

--- app/imports/ui/components/menus/MainMenu.jsx
+++ app/imports/ui/components/menus/MainMenu.jsx
@@ -218,7 +218,7 @@ const MainMenu = ({ user = {} }) => {
           );
         })}
         <Divider />
-        <MenuItem>
+        <MenuItem disabled style={{ opacity: 1 }}>
           <AppVersion />
         </MenuItem>
       </Menu>
