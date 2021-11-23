4cd576ad82730a690d9409c7881d522c116ab272
Francois AUBEUT <francois.aubeut@make.org>
2021-11-23 08:24:21 +0100

    fix: maintenance panel

--- app/imports/ui/pages/admin/AdminHome.jsx
+++ app/imports/ui/pages/admin/AdminHome.jsx
@@ -5,6 +5,7 @@ import Fade from '@material-ui/core/Fade';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import { makeStyles } from '@material-ui/core/styles';
+import { useAppContext } from '../../contexts/context';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -14,13 +15,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function AdminHome() {
+  const [{ isMobile }] = useAppContext();
   const classes = useStyles();
 
   return (
     <Fade in>
       <Container style={{ overflowX: 'auto' }}>
         <Paper className={classes.root}>
-          <Typography variant="h3">{i18n.__('pages.AdminHome.title')}</Typography>
+          <Typography variant={isMobile ? 'h4' : 'h3'}>{i18n.__('pages.AdminHome.title')}</Typography>
           <Typography variant="h6">{i18n.__('pages.AdminHome.subtitle')}</Typography>
         </Paper>
       </Container>
